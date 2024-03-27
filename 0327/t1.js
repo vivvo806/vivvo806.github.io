4a5ffde1af9480308fa13308575d15b9829beaf0
vivvo806 <vvo806@ou.edu>
2024-03-27 10:21:26 -0500

    more buttons

---
+++ 0327/t1.js
@@ -0,0 +1,43 @@
+"use strict";
+let R = null;
+
+// TypeScript
+let t1 = document.querySelector("#t1"); //local global, because there's no {}
+let b1 = document.querySelector("#b1");
+let g1 = null; // global table
+
+async function loads()
+{
+    let url = `https://juxinglong.github.io/static/data/states.json`;
+
+    let r = await fetch(url);
+    let rj = await r.json();
+
+    if (g1)
+    {
+        g1.destroy();
+
+    }
+    t1.innerHTML = ``;
+
+    let opts =
+    {
+        data: rj,
+        width: 500,
+        pagination: { limit: 5 },
+        sort: true,
+        search:true,
+        columns: [{ id: "st", name: "State" }, {id:"p", name:"Population" }]
+    };
+
+    g1 = new gridjs.Grid(opts);
+    g1.render(t1);
+
+    R = rj;
+    console.log(rj);
+
+    //fetch(); // async
+    // Swal.fire("Data Loaded"); //pop-up
+}
+
+b1.addEventListener("click", loads);
